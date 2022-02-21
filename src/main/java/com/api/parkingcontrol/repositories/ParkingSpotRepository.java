20d943da79dfc610fa7d81060812a4b11971b75f
Lucas Silva <lucasoliveirabs95@gmail.com>
2022-02-20 21:36:36 -0300

    add spot and registration plate 'exists' methods to repository interface

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -5,9 +5,13 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.api.parkingcontrol.ParkingSpotModel;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+	boolean existsByRegistrationPlateVehicle(String pRegistrationPlateVehicle);
+
+	boolean existsBySpotId(String pSpotId);
 	
 }
