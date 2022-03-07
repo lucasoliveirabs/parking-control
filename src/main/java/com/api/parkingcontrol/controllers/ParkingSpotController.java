1032321d26a8d1f6b823a6119ce5e408d2b4abd7
Lucas Silva <lucasoliveirabs95@gmail.com>
2022-03-07 20:57:39 -0300

    create delete controller method and set http content handling logic

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +61,15 @@ public class ParkingSpotController {
 			return ResponseEntity.status(HttpStatus.OK).body(optionalParkingSpotModel.get());
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found");
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id){
+		Optional<ParkingSpotModel> optionalParkingSpotModel = parkingSpotService.findById(id);
+		if (!optionalParkingSpotModel.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found");
+		}
+		parkingSpotService.delete(optionalParkingSpotModel.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Parking Spot successfully deleted.");
 	}
 }
